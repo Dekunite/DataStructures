7a4d2ca3931d9f9e1f1661086a756e0ff54ae295
Derviş Kopuz <derviskopuz@gmail.com>
2020-11-07 23:56:43 +0300

    read and count done

--- Codes/src/counter.cpp
+++ Codes/src/counter.cpp
@@ -14,7 +14,21 @@ using namespace std;
 	
 int Counter::findindex(Token *arr,char elem[]){
 	//THIS FUNCTION WILL BE CODED BY YOU
-	return 0;
+	char* order;
+	for (int i = 0; i < Counter::token_count ; i++)
+	{
+		order = strstr(arr[i].token, elem);
+		cout<<order<<endl;
+		if (order != NULL)
+		{
+			return i;
+		}
+		
+	}
+	
+	//int index = strchr(arr->token, elem[0]) - arr->token;
+
+	return -1;
 } 
 
 bool Counter::contains(char *token, char target){
@@ -39,6 +53,10 @@ void Counter::read_and_count(){
 	int k = 0;
 	bool inDecimal = false;
 	Token tempTokenArray[100];
+	Counter::token_count = 0;
+	bool newToken = false;
+	bool *newTokenPtr = &newToken;
+	char isPresent;
 	//int k=0;
 	//char currentToken[2];
 	//char tokens[198];
@@ -55,25 +73,40 @@ void Counter::read_and_count(){
 			//saçma karakterler gelmesin diye
 			tempTokenArray[i-1].token[j+2] = 0;
 
-			for (int l = 0; l < 100; l++)
+			*newTokenPtr = false;
+			for (int l = 0; l <= Counter::token_count; l++)
 			{
-			string counterArray(Counter::token_array[l].token);
-			string token(tempTokenArray[i-1].token);
-			cout << strstr(tempTokenArray[i-1].token,Counter::token_array[l].token) <<endl;
-			if (counterArray.find(token))
+			char* isPresentPtr = &isPresent;
+			isPresentPtr = strstr(Counter::token_array[l].token,tempTokenArray[i-1].token);
+			if (isPresentPtr == NULL)
+			{
+				*newTokenPtr = true;
+			} else
+			{
+				*newTokenPtr = false;
+				Counter::token_array[l].count++;
+				break;
+			}
+			}
+
+			if (newToken)
 			{
 			Counter::token_array[k].token[0] = tempTokenArray[i-1].token[0];
 			Counter::token_array[k].token[1] = tempTokenArray[i-1].token[1];
-			}}
+			Counter::token_array[k].count++;
+			Counter::token_count++;
+			k++;
+			}
+			std::cout << k-1 << ": " << Counter::token_array[k-1].token << endl;
+
 			
-			cout << tempTokenArray[i-1].token<<endl;
 			if (strcmp(tempTokenArray[i-1].token,"35")== 0)
 			{
-				cout<<"found 35 yay" <<endl;
+				std::cout <<"found 35 yay" <<endl;
 			}
 			if (strcmp(tempTokenArray[i-1].token,"9 ")== 0)
 			{
-				cout<<"found 9  yay" <<endl;
+				std::cout <<"found 9  yay" <<endl;
 			}
 			
 			}
@@ -89,10 +122,11 @@ void Counter::read_and_count(){
 			inDecimal = true;
 		}
 		
-		
+		fclose(piApprox);
 	}
+	//findindex(Counter::token_array, "14");
 	
-	contains(&counter.token_array->token[0],*counter.token_array->token);
+	//contains(&counter.token_array->token[0],*counter.token_array->token);
 
 	
 	

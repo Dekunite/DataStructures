4f4964a6254831277af329e224f3920198397491
Derviş Kopuz <derviskopuz@gmail.com>
2020-11-10 00:50:05 +0300

    isim ekle, contains kullan

--- Codes/src/counter.cpp
+++ Codes/src/counter.cpp
@@ -32,12 +32,12 @@ int Counter::findindex(Token *arr,char elem[]){
 
 bool Counter::contains(char *token, char target){
 	//THIS FUNCTION WILL BE CODED BY YOU
-	if (strstr(&target,token))
-	{
-	cout << "true";
-	}
+	if (strcmp(token, &target) == 0)
+		{
+			return true;
+		}
 	
-	return 0;
+	return false;
 }
 
 void Counter::read_and_count(){
@@ -52,13 +52,10 @@ void Counter::read_and_count(){
 	int k = 0;
 	bool inDecimal = false;
 	Token tempTokenArray[100];
-	Counter::token_count = 0;
 	bool newToken = false;
 	bool *newTokenPtr = &newToken;
 	char isPresent;
-	//int k=0;
-	//char currentToken[2];
-	//char tokens[198];
+	
 	while (!feof(piApprox)) {
 		fread(&char1, 1, 1, piApprox);
 		if (inDecimal)
@@ -68,46 +65,34 @@ void Counter::read_and_count(){
 			tempTokenArray[i].token[j] = char1;
 			if (i != 0)
 			{
-			tempTokenArray[i-1].token[j+1] = char1;
-			//saçma karakterler gelmesin diye
-			tempTokenArray[i-1].token[j+2] = 0;
+				tempTokenArray[i-1].token[j+1] = char1;
+				//saçma karakterler gelmesin diye
+				tempTokenArray[i-1].token[j+2] = 0;
 
-			*newTokenPtr = false;
-			for (int l = 0; l <= Counter::token_count; l++)
-			{
-			char* isPresentPtr = &isPresent;
-			isPresentPtr = strstr(Counter::token_array[l].token,tempTokenArray[i-1].token);
-			if (isPresentPtr == NULL)
-			{
-				*newTokenPtr = true;
-			} else
-			{
 				*newTokenPtr = false;
-				Counter::token_array[l].count++;
-				break;
-			}
-			}
+				for (int l = 0; l <= Counter::token_count; l++)
+				{
+					char* isPresentPtr = &isPresent;
+					isPresentPtr = strstr(Counter::token_array[l].token,tempTokenArray[i-1].token);
+					if (isPresentPtr == NULL)
+					{
+						*newTokenPtr = true;
+					} else
+					{
+						*newTokenPtr = false;
+						Counter::token_array[l].count++;
+						break;
+					}
+				}
 
-			if (newToken)
-			{
-			Counter::token_array[k].token[0] = tempTokenArray[i-1].token[0];
-			Counter::token_array[k].token[1] = tempTokenArray[i-1].token[1];
-			Counter::token_array[k].count++;
-			Counter::token_count++;
-			k++;
-			}
-			std::cout << k-1 << ": " << Counter::token_array[k-1].token << endl;
-
-			
-			if (strcmp(tempTokenArray[i-1].token,"35")== 0)
-			{
-				std::cout <<"found 35 yay" <<endl;
-			}
-			if (strcmp(tempTokenArray[i-1].token,"9 ")== 0)
-			{
-				std::cout <<"found 9  yay" <<endl;
-			}
-			
+				if (newToken)
+				{
+					Counter::token_array[k].token[0] = tempTokenArray[i-1].token[0];
+					Counter::token_array[k].token[1] = tempTokenArray[i-1].token[1];
+					Counter::token_array[k].count++;
+					Counter::token_count++;
+					k++;
+				}
 			}
 			i+=1;
 			if (i == fileSize-3)
@@ -120,14 +105,7 @@ void Counter::read_and_count(){
 		{
 			inDecimal = true;
 		}
-		//fclose(piApprox);
 	}
-	//findindex(Counter::token_array, "14");
-	
-	//contains(&counter.token_array->token[0],*counter.token_array->token);
-
-	
-	
 }
 
 
@@ -136,9 +114,6 @@ Token *Counter::get_most_common_three(){
 	int COMMON_SIZE = 3;
 	static Token mostCommon3Tokens[3];
 	Token* ptr;
-	Token* ptr2;
-	Token* ptr3;
-	int currentMax = 0;
 
 	for (int i = 0; i < Counter::token_count; i++)
 	{
@@ -146,28 +121,24 @@ Token *Counter::get_most_common_three(){
 			{
 				if (Counter::token_array[i].count >= mostCommon3Tokens[j].count)
 				{
-						mostCommon3Tokens[j+2] = mostCommon3Tokens[j+1];
-						mostCommon3Tokens[j+1] = mostCommon3Tokens[j];
-
-						mostCommon3Tokens[j].token[0] = Counter::token_array[i].token[0];
-						mostCommon3Tokens[j].token[1] = Counter::token_array[i].token[1];
-						mostCommon3Tokens[j].token[2] = Counter::token_array[i].token[2];
-						mostCommon3Tokens[j].count = Counter::token_array[i].count;
+					if (Counter::token_array[i].token[0] == mostCommon3Tokens[j].token[0]
+					&& Counter::token_array[i].token[1] == mostCommon3Tokens[j].token[1])
+					{
 						break;
+					}
+					mostCommon3Tokens[j+2] = mostCommon3Tokens[j+1];
+					mostCommon3Tokens[j+1] = mostCommon3Tokens[j];
+
+					mostCommon3Tokens[j].token[0] = Counter::token_array[i].token[0];
+					mostCommon3Tokens[j].token[1] = Counter::token_array[i].token[1];
+					mostCommon3Tokens[j].token[2] = Counter::token_array[i].token[2];
+					mostCommon3Tokens[j].count = Counter::token_array[i].count;
+					break;
 				} 
 			}
 	}
 
-	
-	for (int k = 0; k < COMMON_SIZE; k++)
-	{
-	cout << mostCommon3Tokens[k].token << " " << mostCommon3Tokens[k].count <<endl;
-	}
 	ptr = &mostCommon3Tokens[0];
-	ptr2 = &mostCommon3Tokens[1];
-	ptr3 = &mostCommon3Tokens[2];
-
-	Token* common_three[3] = {ptr,ptr2,ptr3};
 
 	return ptr;
 }
